fa8088a0f2e0dd418f64f2427694dcdd75209fca
ikwanpras <prstyoikwan@gmail.com>
2023-01-03 23:07:08 +0700

    COMMIT

--- R/ifa-fgwc ver 1.R
+++ R/ifa-fgwc ver 1.R
@@ -1,9 +1,9 @@
 # Load Data Sovi
-load("D:/My Drive/deden-RMarkdown & Github/FGWC/Data/sovi_data.rdata")
+load("G:/My Drive/SKRIPSI/EXPERIMENT SKRIPSI/FGWC/DATA/sovi_data.rdata")
 # Load Data Matrix Distance
-load("D:/My Drive/deden-RMarkdown & Github/FGWC/Data/sovi_dist.rdata")
+load("G:/My Drive/SKRIPSI/EXPERIMENT SKRIPSI/FGWC/DATA/sovi_dist.rdata")
 #Load Data Populasi
-load("D:/My Drive/deden-RMarkdown & Github/FGWC/Data/sovi_pop.rdata")
+load("G:/My Drive/SKRIPSI/EXPERIMENT SKRIPSI/FGWC/DATA/sovi_pop.rdata")
 
 #Mengubah data frame Sovi distance menjadi matrik distance
 mat_dist= data.matrix(sovi_distance)
@@ -14,7 +14,7 @@ library(rdist)
 
 # Pentuan Parameter fgwc
 param_fgwc <- c(kind='v',ncluster=4,m=2,distance='euclidean',order=3,
-                alpha=0.7,a=1,b=1,max.iter=1000,error=1e-5,randomN=0)
+                alpha=0.7,a=1,b=1,max.iter=1000,error=1e-5,randomN=10)
 
 # Parameter IFA
 ifa_param <- c(vi.dist='uniform', ei.distr='logchaotic',
@@ -27,6 +27,5 @@ Res_ifafgwc <- fgwc(data=sovi_data, pop=Sovi_Pop, distmat=mat_dist,
                     algorithm = "ifa", param_fgwc,ifa_param)
 
 # Menggabungkan hasil cluster algoritma FGWC dengan data
-library(xlsx)
 cluster.output <- cbind(data_kab ,Res_ifafgwc$cluster)
-write.xlsx(cluster.output, file = "Final Cluster.xlsx", row.names = TRUE)
+write.csv(cluster.output, file = "Final Cluster.csv", row.names = TRUE)
