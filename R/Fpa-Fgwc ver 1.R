6d71febfe24fff9f25fb66ee7ddd4f4df2e97842
ikwanpras <prstyoikwan@gmail.com>
2023-01-30 23:51:04 +0700

    initial commit

--- R/Fpa-Fgwc ver 1.R
+++ R/Fpa-Fgwc ver 1.R
@@ -14,7 +14,7 @@ library(rdist)
 
 # Pentuan Parameter fgwc
 param_fgwc <- c(kind='v',ncluster=4,m=2,distance='euclidean',order=3,
-                alpha=0.5,a=1.2,b=1.2,max.iter=1000,error=1e-6,randomN=0)
+                alpha=0.5,a=1.2,b=1.2,max.iter=1000,error=1e-6,randomN=10)
 
 # Parameter FPA
 fpa_param <- c(vi.dist='normal',npar=5,same=15,p=0.7,gamma=1.2,lambda=1.5,
@@ -25,5 +25,5 @@ Res_fpafgwc <- fgwc(data=sovi_data, pop=Sovi_Pop, distmat=mat_dist, algorithm = 
                     param_fgwc,fpa_param)
 
 # Menggabungkan hasil cluster algoritma FGWC dengan data
-cluster.output <- cbind(data_kab ,Res_ifafgwc$cluster)
+cluster.output <- cbind(data_kab ,Res_fpafgwc$cluster)
 write.csv(cluster.output, file = "FPA Final Cluster.csv", row.names = TRUE)
