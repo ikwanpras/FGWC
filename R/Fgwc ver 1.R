79aa4a4ba28fd95efe7158cf530d94ad57985fb4
ikwanpras <prstyoikwan@gmail.com>
2023-01-31 10:36:57 +0700

    initial commit

--- R/Fgwc ver 1.R
+++ R/Fgwc ver 1.R
@@ -14,12 +14,13 @@ library(rdist)
 
 # Pentuan Parameter fgwc
 param_fgwc <- c(kind='v',ncluster=4,m=2,distance='euclidean',order=3,
-                alpha=0.5,a=1.2,b=1.2,max.iter=1000,error=1e-6,randomN=0)
+                alpha=0.5,a=1.2,b=1.2,max.iter=1000,error=1e-6,randomN=10)
 
 # FGWC Standart
 Res_fgwc <- fgwc(data=sovi_data, pop=Sovi_Pop, distmat=mat_dist, algorithm = "classic", param_fgwc,1)
 
+
 # Menggabungkan hasil cluster algoritma FGWC dengan data
-library(xlsx)
 cluster.output <- cbind(data_kab ,Res_fgwc$cluster)
-write.xlsx(cluster.output, file = "Final Cluster.xlsx", row.names = TRUE)
+write.csv(cluster.output, file = "FGWC Final Cluster.csv", row.names = TRUE)
+
